a5ec99bd4631803b0f78cfe78165c781c0a5a0fa
shukla01akash <as1999shukla01@gmail.com>
2022-07-26 15:22:47 +0530

    Add Project

---
+++ Assignment26/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment26.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EmployeeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult EmployeePage()
+        {
+            return View();
+        }
+        public ActionResult Congratulation()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment26/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment26.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
